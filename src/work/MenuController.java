3446fedd5381f5ac44b26f127d6ff513eadd19d7
2103962345 <2103962345@mail.ru>
2017-04-14 18:01:20 +0300

    Second commit

--- src/work/MenuController.java
+++ src/work/MenuController.java
@@ -23,8 +23,8 @@ public class MenuController {
 				    input = sc.nextLine();
 				}
 			 lines = input.split(" ");
-			 if(lines.length>=2)
-			 switch(lines[2].toLowerCase()){
+			 if(lines.length>=1)
+			 switch(lines[0].toLowerCase()){
 			 case "add": { 
 				 add();
 				 break;
@@ -44,6 +44,10 @@ public class MenuController {
 			 case "printf": { 
 				 printf();
 				 break; 
+				 }
+			 case "help" : {
+				 help();
+				 break;
 				 }
 			 case "exit" : {
 				 notExit = false;
@@ -73,12 +77,13 @@ public class MenuController {
 	 
 	 //Добавить статус или новую книгу/музыку/фильм
 	 public void add(){
-		 //$ manager add status <name>
+		 //add status <name>
 		 listStatus = new Vector<Information>();
 		 int k=1;
-		 if(lines.length>=5 && lines[3].toLowerCase().equals("status")){
-			 name = takeName(4);
-			 if(!name.equals("") && list.size()!=0){
+		 if(lines.length>=3 && lines[1].toLowerCase().equals("status")){
+			 if(list.size()!=0){
+			 name = takeName(2);
+			 if(!name.equals("")){
 				 //вывод всех найденных объектов с введенным именем
 			 for(int i=0;i<list.size();i++)
 				 if(name.equals(list.get(i).getName())){
@@ -107,11 +112,12 @@ public class MenuController {
 					 fl = false;
 					 }
 				 else i++;
-			 }
+			 } else System.out.println("Wrong command (not enough arguments).");
+			 } else System.out.println("Add some objects");
 		 }
-		 else if(lines.length>=4 && !lines[3].toLowerCase().equals("status")){
-			 // $ manager add <name>
-			 name=takeName(3);
+		 else if(lines.length>=2 && !lines[1].toLowerCase().equals("status")){
+			 // add <name>
+			 name=takeName(1);
 			 if(!name.equals("")){
 			 System.out.println("What is "+name+"?");
 			 System.out.println("1. Book");
@@ -127,48 +133,49 @@ public class MenuController {
 							if (j<1 || j>3) 
 						        throw new IllegalArgumentException("Enter a number 1, 2 or 3.");
 							else { 
-								if(j==1)
+								if(j==1 && search("Book",name))
 									 list.addElement(new Book(name));
-								 else if(j==2) 
+								 else if(j==2 && search("Film",name)) 
 									 list.addElement(new Film(name));
-								 else if (j==3) 
+								 else if (j==3 && search("Music",name)) 
 									 list.addElement(new Music(name));
 							flag = false;
 							}
 							}catch (InputMismatchException e){
-								System.out.println("Неверное значение!");
+								System.out.println("Wrong number!");
 								in.next();
 								}
 						catch(IllegalArgumentException e){
 							System.out.println(e.getMessage());
 						}
 					} 
-			}
-		 }
+			} else System.out.println("Wrong command (not enough arguments).");
+		 } else System.out.println("Wrong command.");
 		 
 	 }
 	 
-	 //$ manager delete <type> <name>
+	 //delete <type> <name>
 	 public void delete(){
-		 if(lines.length>=5){
-		 name=takeName(4);
-		 int i=0;
 		 boolean flag = true;
-		 if(!lines[3].equals("") && !name.equals("") && list.size()!=0){
+		 if(lines.length>=3){
+		 name=takeName(2);
+		 int i=0;
+		 if(list.size()!=0){
+		 if(!lines[1].equals("") && !name.equals("")){
 		 while(i<list.size() && flag){
-			 if(lines[3].toLowerCase().contains("book")){
+			 if(lines[1].toLowerCase().contains("book")){
 				 if((list.get(i) instanceof Book)&& name.equals(list.get(i).getName())){
 					 list.remove(i);
 					 flag = false;
 				 }
 				 }
-			 else if(lines[3].toLowerCase().contains("music")){
+			 else if(lines[1].toLowerCase().contains("music")){
 				 if((list.get(i) instanceof Music)&& name.equals(list.get(i).getName())){
 					 list.remove(i);
 					 flag = false;
 				 }
 				 }
-			 else if(lines[3].toLowerCase().contains("film")){
+			 else if(lines[1].toLowerCase().contains("film")){
 				 if((list.get(i) instanceof Film)&& name.equals(list.get(i).getName())){
 					 list.remove(i);
 					 flag = false;
@@ -176,90 +183,72 @@ public class MenuController {
 				 }
 			 i++;
 			 }
-		 }
-		 }
+		 } else System.out.println("Wrong command.");
+		 }else System.out.println("Add some objects.");
+		 } else System.out.println("Wrong command (not enough arguments).");
+		 if (flag) System.out.println("Wrong command.");
 		 }
 	 
-	 //$ manager print <type>
+	 //print <type>
 	 public void print(){
 		 int j=1;
-		 if(lines.length>=4){
-		 if(!lines[3].equals("") && list.size()!=0){
-		 if(lines[3].toLowerCase().contains("book")){
+		 if(lines.length>=2){
+			 if(list.size()!=0){
+		 if(!lines[1].equals("")){
+		 if(lines[1].toLowerCase().contains("book")){
 			 for(int i=0;i<list.size();i++)
 				 if(list.get(i) instanceof Book){
 					 System.out.print(j+++". ");
 					 list.get(i).print();
 				 }
 			 }
-		 else if(lines[3].toLowerCase().contains("music")){
+		 else if(lines[1].toLowerCase().contains("music")){
 			 for(int i=0;i<list.size();i++)
 				 if(list.get(i) instanceof Music){
 					 System.out.print(j+++". ");
 					 list.get(i).print();
 				 }
 			 }
-		 else if(lines[3].toLowerCase().contains("film")){
+		 else if(lines[1].toLowerCase().contains("film")){
 			 for(int i=0;i<list.size();i++)
 				 if(list.get(i) instanceof Film){
 					 System.out.print(j+++". ");
 					 list.get(i).print();
 				 }
-			 }
-		 }
-		 }
+			 } else System.out.println("Wrong command.");
+		 }else System.out.println("Wrong command.");
+		 } else System.out.println("Add some objects.");
+		 } else System.out.println("Wrong command.");
 }
-	// $ manager printf <type> <name>
+	// printf <status>
 	 public void printf(){
 		 int j=1;
 		 String stat ="";
-		 if(lines.length>=5){
+		 if(lines.length>=2){
 		 if(list.size()!=0){
-		 if(lines.length ==5){
-			 if(!lines[3].equals("")&& !lines[4].equals(""))
-				 stat = lines[4].toUpperCase();
+		 if(lines.length ==2){
+			 if(!lines[1].equals(""))
+				 stat = lines[1].toUpperCase();
 		 }
-		 else if(lines.length==6){
-			 if(!lines[3].equals("") && !lines[4].equals("") && !lines[5].equals(""))
-				 stat = lines[4].toUpperCase()+" "+lines[5].toUpperCase();
+		 else if(lines.length==3){
+			 if(!lines[1].equals("") && !lines[2].equals(""))
+				 stat = lines[1].toUpperCase()+" "+lines[2].toUpperCase();
 		 }
-		 
-		 if(lines[3].toLowerCase().contains("book")){
 			 for(int i=0;i<list.size();i++){
-				 if((list.get(i) instanceof Book))
 					 if(list.get(i).getStatus().equals(stat)){
 						 System.out.print(j+++". ");
 						 list.get(i).print();
 					 }
 				 }
-			 }
-		 else if(lines[3].toLowerCase().contains("music")){
-			 for(int i=0;i<list.size();i++){
-				 if((list.get(i) instanceof Music))
-					 if(list.get(i).getStatus().equals(stat)){
-						 System.out.print(j+++". ");
-						 list.get(i).print();
-					 }
-				 }
-			 }
-		 else if(lines[3].toLowerCase().contains("film")){
-			 for(int i=0;i<list.size();i++){
-				 if((list.get(i) instanceof Film))
-					 if(list.get(i).getStatus().equals(stat)){
-						 System.out.print(j+++". ");
-						 list.get(i).print();
-					 }
-				 }
-			 }
-		 }
-		 }
+		 } else System.out.println("Add some objects.");
+		 } else System.out.println("Wrong command (not enough arguments).");
 	 }
 		 
-	 // $ manager status <name>
+	 // status <name>
 	 public void status(){
 		 int j=1;
-		 if(lines.length>=4)
-		 name=takeName(3);
+		 if(lines.length>=2)
+		 name=takeName(1);
 		 if(!name.equals(""))
 		 for(Information element : list){
 			 if(name.equals(element.getName())){
@@ -280,13 +269,55 @@ public class MenuController {
 					        throw new IllegalArgumentException("Error! Type a number and status");
 					else return n;
 					}catch (InputMismatchException e){
-						System.out.println("Неверное значение!");
+						System.out.println("Wrong number!");
 						in.next();
-					
 						}
 				catch(IllegalArgumentException e){
 					System.out.println(e.getMessage());
 				}
 			}
 		}
+	 //help
+	 public void help(){
+		 System.out.println("Commands: ");
+		 System.out.println("\t - for adding book, music or film type");
+		 System.out.println("add <name>");
+		 System.out.println("\t - for adding status for book, music or film type ");
+		 System.out.println("add status <name>");
+		 System.out.println("\t - to delete book, music or film type");
+		 System.out.println("delete <type> <name>");
+		 System.out.println("\t - to show all books, music or films type ");
+		 System.out.println("print <type>");
+		 System.out.println("\t - to show all books, music or films with specific status type ");
+		 System.out.println("printf <status>");
+		 System.out.println("\t - to show status for specific book, music or film with type ");
+		 System.out.println("status <name>");
+		 System.out.println("\t - for help type ");
+		 System.out.println("help");
+		 System.out.println("You can add 3 different type");
+		 System.out.println("For books: ");
+		 System.out.println("\t * reading ");
+		 System.out.println("\t * want read (default) ");
+		 System.out.println("\t * read ");
+		 System.out.println("For films: ");
+		 System.out.println("\t * watching ");
+		 System.out.println("\t * want watch (default) ");
+		 System.out.println("\t * watched ");
+		 System.out.println("For music: ");
+		 System.out.println("\t * listening ");
+		 System.out.println("\t * want listen (default) ");
+		 System.out.println("\t * listened ");
+	 }
+	 
+	 public boolean search(String className,String name){
+		 boolean response = true;
+		 int i=0;
+		 while(i<list.size() && response){
+			 if(list.get(i).getClass().toString().contains(className) && list.get(i).getName().equals(name)){
+				 response = false;
+				 break;
+			 } else i++;
+		 }
+		 return response;
+	 }
 }
